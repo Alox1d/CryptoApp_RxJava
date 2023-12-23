f565f4830dab4b023ee85f3b72a5d466dca5ff6b
Alox1d <alox1dab@gmail.com>
2023-12-24 01:12:01 +0300

    added RefreshDataWorkerFactory to pass params via injection by Dagger2

--- app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -1,10 +1,28 @@
 package com.example.cryptoapp.presentation
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorker
+import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerAppComponent
 
-class CoinApp: Application() {
+class CoinApp : Application(), Configuration.Provider {
+
     val component by lazy {
         DaggerAppComponent.factory().create(this)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    AppDatabase.getInstance(this).coinPriceInfoDao(),
+                    ApiFactory.apiService,
+                    CoinMapper()
+                )
+            ).build()
     }
 }
